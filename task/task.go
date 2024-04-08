0e5d37cb21d813e04d509d471ab33ea14013a05c
Rahul More <rahulmore@Rahuls-MacBook-Air.local>
2024-04-07 21:37:44 -0600

    Task and TaskEvent are not defined

--- task/task.go
+++ task/task.go
@@ -1,5 +1,11 @@
 package task
 
+import (
+	"github.com/docker/go-connections/nat"
+	"github.com/google/uuid"
+	"time"
+)
+
 type State int
 
 const (
@@ -9,3 +15,24 @@ const (
 	Completed
 	Failed
 )
+
+type Task struct {
+	ID            uuid.UUID
+	Name          string
+	State         State
+	Image         string
+	Memory        int
+	Disk          int
+	ExposedPorts  nat.PortSet
+	PortBindings  map[string]string
+	RestartPolicy string
+	StartTime     time.Time
+	FinishTime    time.Time
+}
+
+type TaskEvent struct {
+	ID        uuid.UUID
+	State     State
+	TimeStamp time.Time
+	Task      Task
+}
