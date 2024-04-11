b8e5383c5a2716c0213983e1c5d3baae036b216a
Rahul More <rahulmore@Rahuls-MacBook-Air.local>
2024-04-10 21:02:34 -0600

    workers

--- manager/manager.go
+++ manager/manager.go
@@ -1,1 +1,29 @@
 package manager
+
+import (
+	"cude/task"
+	"fmt"
+	"github.com/golang-collections/collections/queue"
+	"github.com/google/uuid"
+)
+
+type Manager struct {
+	Pending       queue.Queue
+	TaskDb        map[string][]*task.Task
+	EventDb       map[string][]*task.TaskEvent
+	Workers       []string
+	WorkerTaskMap map[string][]uuid.UUID
+	TaskWorkerMap map[uuid.UUID]string
+}
+
+func (m *Manager) SelectWorker() {
+	fmt.Println("I will select an appropriate Worker")
+}
+
+func (m *Manager) UpdateTask() {
+	fmt.Println("I will Update the tasks")
+}
+
+func (m *Manager) SendWork() {
+	fmt.Println("I will send work to the workers")
+}
