8df0e342d246871e79f1e7fcb102fe78c772f906
Rahul More <rahulmore@Rahuls-MacBook-Air.local>
2024-04-14 20:34:43 -0600

    Add type to Node

--- node/node.go
+++ node/node.go
@@ -1,1 +1,15 @@
+// Represents a machine in a cluster
+
 package node
+
+type Node struct {
+	Name            string
+	Ip              string
+	Cores           int
+	Memory          int
+	MemoryAllocated int
+	Disk            int
+	DiskAllocated   int
+	TaskCount       int
+	Role            string
+}
