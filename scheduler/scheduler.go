2317d1fd4ecc042cc0c8f8f8cb6bf66edd0910b7
Rahul More <rahulmore@Rahuls-MacBook-Air.local>
2024-04-14 20:01:05 -0600

    Add interface to Scheduler

--- scheduler/scheduler.go
+++ scheduler/scheduler.go
@@ -1,1 +1,10 @@
+// Select the worker on which the task can be run
+// Score candidates
+// Pick candidates from good to worse
 package scheduler
+
+type Scheduler interface {
+	SelectCandidateNodes()
+	Score()
+	Pick()
+}
